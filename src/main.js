1aa3c517e0a6df3609306005557008b4acb0e6af
Ykoss <45798271@qq.com>
2018-10-09 21:58:32 +0800

    买买买第一天

--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 导入element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+// 路由相关
+import VueRouter from 'vue-router'
+// 导入index组件
+import index from './components/index.vue'
+Vue.use(VueRouter);
+// 写路由
+const routes = [
+  {
+    path:'/',
+    component:index
+  },
+  {
+    path:'/index',
+    component:index
+  }
+]
+
+// 实例化路由对象
+const router = new VueRouter({
+  routes
+})
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  // 挂在到Vue实例
+  router
 }).$mount('#app')
